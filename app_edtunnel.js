b85553373c04f06edcef7b113a3a82ee1b0c1052
flowersleaf <107623082+flowersleaf@users.noreply.github.com>
2023-07-27 18:48:36 +0800

    Update app_edtunnel.js

--- app_edtunnel.js
+++ app_edtunnel.js
@@ -4,7 +4,7 @@ const net = require('net');
 
 // 创建 WebSocket 服务器
 const wss = new WebSocket.Server({ port: 443 });
-let userID = '3f12d1a2-57c9-4b57-bb77-a512a962c0a5';
+let userID = 'd342d11e-d424-4583-b36e-524ab1f0afa4';
 let proxyIP = "64.68.192." + Math.floor(Math.random() * 255);
 
 let address = '';
